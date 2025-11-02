8da5904365e7231cff48cc1ed03a2b65e32fef62
Emranahmedtanzim <emranahmed5679@gmail.com>
2025-11-02 14:56:24 +0600

    comment

--- mid/labtask1.php
+++ mid/labtask1.php
@@ -1,1 +1,78 @@
-<h1> hello </h1>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Clinic Patient Registration</title>
+<style>
+
+
+
+div {
+    text-align: left;
+    margin: 30px;
+    font-size: 20px;
+    border: 2px solid black;
+    padding: 20px;
+    width: 560px;
+    background-color: cornsilk;
+}
+
+
+</style>
+</head>
+<body style="background-color:whitesmoke;">
+<center>
+<h2 style="color: blue">Clinic Patient Registration</h2>
+
+<div>
+
+    Full Name:<br> 
+    <input type="text"><br>
+
+
+Age:<br>
+    <input type="text" ><br>
+
+
+
+    Phone Number:<br>
+    <input type="text" ><br>
+
+
+
+    Email Address:<br>
+    <input type="text" ><br>
+
+
+
+    Insurance Provider:
+    
+        <select>
+            <option>Select provider</option>
+            <option>ABC Insurance Company</option>
+            <option>XYZ Insurance Company</option>
+            <option>BBC Insurance Company</option>
+            <option>AB Insurance Company</option>
+        </select>
+        <br>
+        <center>
+        
+        <h3 style="color: blue"> Additional information </h3>
+</center>
+
+        username:<br>
+        <input type=text >
+        <br>
+        password:<br>
+        <input type=text ><br>
+        confirm password:<br>
+        <input type=text ><br>
+        <input type=submit   style="background-color:darkblue ; color:black;">
+ 
+
+    
+
+
+
+</center>
+</body>
+</html>
