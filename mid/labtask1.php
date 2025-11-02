539d13031cef4dadf49da0451828c84cd1b3c1ce
Emranahmedtanzim <emranahmed5679@gmail.com>
2025-11-02 13:15:37 +0600

    comment

--- mid/labtask1.php
+++ mid/labtask1.php
@@ -3,7 +3,7 @@
     <head>
         <title>aiub portal</title>
     </head>
-    <body style=>
+    <body>
     <h1 style="color: red;">hello labtask:</h1>
     <h1> hello enter your enter </h1>
     </body>
