7c665a72e9a08d381ee0d2f0d526043d1c203353
Emranahmedtanzim <emranahmed5679@gmail.com>
2025-11-15 04:32:28 +0600

    comment

--- mid/labtask2.php
+++ mid/labtask2.php
@@ -5,15 +5,53 @@
     </head>
     <style>
         div{
-            width:300px;
-            margin:auto;
+            text-align:left;
+            margin:30px;
+            font-size:20px;
+            border:2px solid black;
+            padding:20px;
+            width:560px;
+            background-color: cornsilk;
+        }
+        h2.title1{
+            color:blue
         }
     </style>
     <body>
-        <h2>blood donation registration</h2>
+        <center>
+        <h2 class="title1">blood donation registration</h2>
+        </center>
+        <center>
         <div>
-            fullname:
-            <input type="text">
-        </div>
+            
+            fullname:<br>
+            <input type="text" style="width: 560px;"><br>
+            number:<br>
+            <input type="text" style="width:560px;"><br>
+            Address:
+            <input type="text" style="width: 560px;"><br>
+            give your insurance info:
+            <br>
+<select style="width: 560px;">
+    <center>
+    <option>--select insurance--</option>
+    <option>Health Insurance</option>
+    <option>Life Insurance</option>
+    <option>Car Insurance</option>
+    </center>
+</select>
+<center>
+ <h2 style="color:blue">conform your address</h2>
+</center>
+password:<br>
+<input type="text" style="width:560px">
+new password:<br>
+<input type="text" style="width:560px">
+<br><br>
+<button style="width:560px; color:darkblue; padding:10px; background-color:darkblue;" >
+
+<br>
+    </div>
+        </center>
     </body>
 </html>
