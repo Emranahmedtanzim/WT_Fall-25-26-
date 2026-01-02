fc3e62af37e730a4d91a7279f54f7d93f67ebfe8
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-02 16:26:51 +0600

    head done

--- project/buyer/veiw/html files/login(html).php
+++ project/buyer/veiw/html files/login(html).php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login - HomeFinder</title>
-    <!-- Link to CSS -->
-    <link rel="stylesheet" href="login.css">
+    <title>Login Page</title>
+
+    <
+    <link rel="stylesheet" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/css%20files/login.css">
+    <link rel="icon" type="image/jpeg" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpageiconpicture.png">
 </head>
 <body>
 
-    <!-- Main login page image like dashboard bg -->
-    <img src="loginpagepicture.jpeg" alt="Login Image" id="bg">
-
-    <!-- Header / logo -->
-    <img src="loginpageiconpicture.png" alt="Icon" id="icon">
-
-    <!-- Header text -->
+    
     <div class="page-header">
+        
+        <img src="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpageiconpicture.png" 
+             alt="Icon" class="header-icon">
         <h1>Welcome to HomeFinder</h1>
-        <p>Helping you find your dream home</p>
+        <p>Helping you to find a better home</p>
     </div>
 
-    <!-- Login Box -->
+    
     <div class="login-box">
         <h2>Login</h2>
-        <input type="text" placeholder="Username"><br>
-        <input type="password" placeholder="Password"><br>
+        <input type="text" placeholder="Username"><br><br>
+        <input type="password" placeholder="Password"><br><br>
         <button class="login-btn">Login</button>
     </div>
 
-    <!-- Signup Section -->
+    <
     <div class="signup-outside">
-        <p>Don’t have an account?</p>
+        <div>Don’t have an account?</div>
         <button class="signup-btn">Sign Up</button>
     </div>
 
+    
+    <img src="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpagepicture.jpeg" 
+         alt="Background" class="bg-image">
+
 </body>
 </html>
-
-
