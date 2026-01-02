32f155bb014e2fbebce90a882e4086a0812d4941
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-02 16:27:14 +0600

    head done

--- project/buyer/veiw/html files/login(html).php
+++ project/buyer/veiw/html files/login(html).php
@@ -1,40 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Login Page</title>
-
-    <
-    <link rel="stylesheet" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/css%20files/login.css">
-    <link rel="icon" type="image/jpeg" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpageiconpicture.png">
-</head>
-<body>
-
-    
-    <div class="page-header">
+    <head>
+        <title>login page</title>
+        <link rel="stylesheet" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/css%20files/login.css">
+        <link rel="icon" href="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpageiconpicture.png">
+      </head>
+      <body>
         
-        <img src="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpageiconpicture.png" 
-             alt="Icon" class="header-icon">
-        <h1>Welcome to HomeFinder</h1>
-        <p>Helping you to find a better home</p>
-    </div>
-
-    
-    <div class="login-box">
-        <h2>Login</h2>
-        <input type="text" placeholder="Username"><br><br>
-        <input type="password" placeholder="Password"><br><br>
-        <button class="login-btn">Login</button>
-    </div>
-
-    <
-    <div class="signup-outside">
-        <div>Don’t have an account?</div>
-        <button class="signup-btn">Sign Up</button>
-    </div>
-
-    
-    <img src="http://localhost/WT_Fall-25-26-/project/buyer/veiw/image/loginpagepicture.jpeg" 
-         alt="Background" class="bg-image">
-
-</body>
+      </body>
 </html>
