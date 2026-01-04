908c9e70401ac4e46028f79ff59f4465af9d0b46
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-04 10:41:28 +0600

    buyer register change

--- project/buyer/buyerregister.php
+++ project/buyer/buyerregister.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Buyer Login</title>
+    <title>Buyer Registration</title>
     <link rel="stylesheet" href="auth.css">
 </head>
-
 <body>
 
 <div class="auth-box">
-    <h2>Buyer Login</h2>
+    <h2>Buyer Registration</h2>
 
     <form method="POST" action="">
+        <input type="text" name="fullname" placeholder="Full Name" required>
         <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="password" placeholder="Password" required minlength="6">
 
-        <button type="submit" name="login">Login</button>
+        <button type="submit" name="register">Register</button>
     </form>
 
     <p class="link-text">
-        New buyer?
-        <a href="index.php?page=buyerregister">Register</a>
+        Already have an account?
+        <a href="index.php?page=buyerlogin">Login</a>
     </p>
 </div>
 
