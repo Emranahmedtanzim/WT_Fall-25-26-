48439ccb96d53acbbfdd3cef9452424fbc6b4498
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-04 06:21:28 +0600

    just creationg a draft from my practice i will copy paste this one

---
+++ project/buyer/buyerregister.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buyer Registration</title>
+   
+</head>
+<body>
+
+<div class="auth-box">
+    <h2>Buyer Registration</h2>
+
+    <form method="POST" action="">
+        <input type="text" name="name" placeholder="Full Name" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+
+        <button type="submit" name="register">Register</button>
+    </form>
+
+    
+</div>
+
+</body>
+</html>
