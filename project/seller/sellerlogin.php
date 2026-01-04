1757dccc613b698cfe1669cf703ebe57cac95dc9
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-04 06:23:29 +0600

    same as buyer just some small change here

---
+++ project/seller/sellerlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Seller Login</title>
+    <link rel="stylesheet" href="auth.css">
+</head>
+<body>
+
+<div class="auth-box">
+    <h2>Seller Login</h2>
+
+    <form method="POST" action="">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+
+        <button type="submit" name="login">Login</button>
+    </form>
+
+    <p class="link-text">
+        New seller?
+        <a href="seller-register.php">Register</a>
+    </p>
+</div>
+
+</body>
+</html>
