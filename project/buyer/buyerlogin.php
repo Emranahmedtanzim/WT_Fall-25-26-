7dd538c84cb31b52ddb7a9c5b751bebb3b37850d
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-04 10:38:32 +0600

    buyerlogin page change in code

--- project/buyer/buyerlogin.php
+++ project/buyer/buyerlogin.php
@@ -1,17 +1,28 @@
-<link rel="stylesheet" href="auth.css">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buyer Login</title>
+    <link rel="stylesheet" href="auth.css">
+</head>
+
+<body>
 
 <div class="auth-box">
     <h2>Buyer Login</h2>
 
-    
+    <form method="POST" action="">
         <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
+        <input type="password" name="password" placeholder="Password" required minlength="6">
 
         <button type="submit" name="login">Login</button>
-    
+    </form>
 
     <p class="link-text">
         New buyer?
         <a href="index.php?page=buyerregister">Register</a>
     </p>
 </div>
+
+</body>
+</html>
+
