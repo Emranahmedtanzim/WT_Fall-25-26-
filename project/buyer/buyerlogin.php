cc0940f873f94f72f55259ba730a976e90a42add
Emranahmedtanzim <emranahmed5679@gmail.com>
2026-01-04 06:15:56 +0600

    creating the form here still need some work to do

--- project/buyer/buyerlogin.php
+++ project/buyer/buyerlogin.php
@@ -4,3 +4,12 @@
     <title>Buyer Login</title>
     <link rel="stylesheet" href="auth.css">
 </head>
+<div class="auth-box">
+    <h2>Buyer Login</h2>
+
+    <form method="POST" action="">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+
+        <button type="submit" name="login">Login</button>
+    </form>
